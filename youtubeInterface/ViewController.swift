068a54da0df3aca63acfe4b834c32480cd3a23e5
Wilson Benavides <benavidesb@unicauca.edu.co>
2018-05-13 00:40:39 -0700

    return 5 items of collection view

--- youtubeInterface/ViewController.swift
+++ youtubeInterface/ViewController.swift
@@ -16,8 +16,17 @@ class HomeController: UICollectionViewController {
         navigationItem.title = "Home"
         
         collectionView?.backgroundColor = .white
+        
+        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellId")
     }
     
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
     
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
+        return cell
+    }
 }
 
