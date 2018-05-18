49e9be6bbe717afd7cc8fae1a1914fa921975194
Wilson Benavides <benavidesb@unicauca.edu.co>
2018-05-17 23:09:41 -0700

    ep 08 How to Create a Slide-In Menu Part 2

--- youtubeInterface/Controller/SettingLauncher.swift
+++ youtubeInterface/Controller/SettingLauncher.swift
@@ -8,7 +8,17 @@
 
 import UIKit
 
-class SettingsLauncher: NSObject {
+class Setting: NSObject {
+    let name: String
+    let imageName: String
+    
+    init(name: String, imageName: String) {
+        self.name = name
+        self.imageName = imageName
+    }
+}
+
+class SettingsLauncher: NSObject, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     
     let blackView = UIView()
     
@@ -19,8 +29,15 @@ class SettingsLauncher: NSObject {
         return cv
     }()
     
+    let cellId = "cellId"
+    let cellHeight : CGFloat = 50
+    
+    let settings: [Setting] = {
+        return [Setting(name: "Settings", imageName: "settings"), Setting(name: "Terms & Privacy policy", imageName: "privacy"), Setting(name: "Send Feedback", imageName: "feedback"), Setting(name: "Help", imageName: "help"), Setting(name: "Switch Account", imageName: "switch_account"), Setting(name: "Cancel", imageName: "cancel")]
+    }()
+    
     func showSettings() {
-        //show menu
+        
         if let window = UIApplication.shared.keyWindow {
             
             blackView.backgroundColor = UIColor(white: 0, alpha: 0.5)
@@ -30,14 +47,14 @@ class SettingsLauncher: NSObject {
             window.addSubview(blackView)
             window.addSubview(collectionView)
             
-            let height : CGFloat = 200
+            let height : CGFloat = CGFloat(settings.count) * cellHeight
             let y = window.frame.height - height
             collectionView.frame = CGRect(x: 0, y: window.frame.height, width: window.frame.width, height: height)
             
             blackView.frame = window.frame
             blackView.alpha = 0
             
-            UIView.animate(withDuration: 1.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
                 self.blackView.alpha = 1
                 
                 self.collectionView.frame = CGRect(x: 0, y: y, width: self.collectionView.frame.width, height: self.collectionView.frame.height)
@@ -55,8 +72,33 @@ class SettingsLauncher: NSObject {
         }
     }
     
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return settings.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! SettingCell
+        
+        let setting = settings[indexPath.item]
+        cell.setting = setting
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: cellHeight)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
     override init() {
         super.init()
-        //start doing something here maybe...
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        collectionView.register(SettingCell.self, forCellWithReuseIdentifier: cellId)
     }
 }
---
+++ youtubeInterface/View/SettingCell.swift
@@ -0,0 +1,60 @@
+//
+//  SettingCell.swift
+//  youtubeInterface
+//
+//  Created by willix on 17/05/18.
+//  Copyright © 2018 willix. All rights reserved.
+//
+
+import UIKit
+
+class SettingCell: BaseCell {
+    
+    override var isHighlighted: Bool {
+        didSet {
+            backgroundColor = isHighlighted ? .darkGray : .white
+            nameLabel.textColor = isHighlighted ? .white : .black
+            iconImageView.tintColor = isHighlighted ? .white : .darkGray
+            print(isHighlighted)
+        }
+    }
+    
+    var setting : Setting? {
+        didSet {
+            nameLabel.text = setting?.name
+            
+            if let imageName = setting?.imageName {
+                iconImageView.image = UIImage(named: imageName)?.withRenderingMode(.alwaysTemplate)
+                iconImageView.tintColor = .darkGray
+            }
+        }
+    }
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Setting"                                                              
+        label.font = UIFont.systemFont(ofSize: 13)
+        return label
+    }()
+    
+    let iconImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "settings")
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    override func setupViews() {
+        super.setupViews()
+        
+        addSubview(nameLabel)
+        addSubview(iconImageView)
+        
+        addConstraintsWithFormat(format: "H:|-8-[v0(30)]-8-[v1]|", views: iconImageView, nameLabel)
+        addConstraintsWithFormat(format: "V:|[v0]|", views: nameLabel)
+        addConstraintsWithFormat(format: "V:[v0(30)]", views: iconImageView)
+        
+        addConstraint(NSLayoutConstraint(item: iconImageView, attribute: .centerY, relatedBy: .equal, toItem: self, attribute: .centerY, multiplier: 1, constant: 0))
+    }
+}
+
